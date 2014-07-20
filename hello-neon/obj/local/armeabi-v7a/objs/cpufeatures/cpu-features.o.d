45abb257a8261900e9a532edd5068e0e6609268d
invisiable <ljp@meitu.com>
2014-07-20 21:02:53 +0800

    上传初始版本代码

---
+++ hello-neon/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o.d
@@ -0,0 +1,170 @@
+obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o: \
+ E:/开发工具/Android/android-ndk-r9d/sources//android/cpufeatures/cpu-features.c \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/system_properties.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/android/api-level.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/pthread.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/time.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/time.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdint.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/time.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/siginfo.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/siginfo.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/signal.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/limits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/limits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/limits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/internal_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/limits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/syslimits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/page.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/string.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/malloc.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/signal.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/signal.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sched.h \
+ E:/开发工具/Android/android-ndk-r9d/sources//android/cpufeatures/cpu-features.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdio.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdlib.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/alloca.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/strings.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/memory.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/fcntl.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/fcntl.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/fcntl.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/fcntl.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/unistd.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/select.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/sysconf.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/capability.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/pathconf.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/errno.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/errno.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/errno.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/errno.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/errno-base.h
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/system_properties.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/android/api-level.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/pthread.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/time.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/time.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdint.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/time.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/siginfo.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/siginfo.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/signal.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/limits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/limits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/limits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/internal_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/limits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/syslimits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/page.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/string.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/malloc.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/signal.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/signal.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sched.h:
+
+E:/开发工具/Android/android-ndk-r9d/sources//android/cpufeatures/cpu-features.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdio.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdlib.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/alloca.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/strings.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/memory.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/fcntl.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/fcntl.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/fcntl.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/fcntl.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/unistd.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/select.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/sysconf.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/capability.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/pathconf.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/errno.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/errno.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/errno.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/errno.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/errno-base.h:
---
+++ hello-neon/obj/local/armeabi-v7a/objs/helloneon/helloneon.o.d
@@ -0,0 +1,94 @@
+obj/local/armeabi-v7a/objs/helloneon/helloneon.o: jni/helloneon.c \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/jni.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/android/api-level.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/time.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/time.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdint.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/posix_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/stddef.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/compiler.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/posix_types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/types.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/kernel.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/time.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/siginfo.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/siginfo.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdio.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdlib.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/string.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/malloc.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/alloca.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/strings.h \
+ E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/memory.h \
+ E:/开发工具/Android/android-ndk-r9d/sources//android/cpufeatures/cpu-features.h \
+ jni/helloneon-intrinsics.h
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/jni.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/android/api-level.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/time.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/time.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdint.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/posix_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/stddef.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/compiler.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/posix_types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/types.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/machine/kernel.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/linux/time.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm/siginfo.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/asm-generic/siginfo.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdio.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/stdlib.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/string.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/malloc.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/alloca.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/strings.h:
+
+E:/开发工具/Android/android-ndk-r9d/platforms/android-4/arch-arm/usr/include/memory.h:
+
+E:/开发工具/Android/android-ndk-r9d/sources//android/cpufeatures/cpu-features.h:
+
+jni/helloneon-intrinsics.h:
